cab17af87482d9fe8521a02cac327d455365e908
Hugo Cortes <contact@hugocortes.dev>
2022-07-20 09:54:57 -0700

    example

--- tools/docker/launch.sh
+++ tools/docker/launch.sh
@@ -10,11 +10,10 @@ npm run generate:realm
 docker run -d \
   --name keycloakify \
   -e DB_VENDOR=h2 \
-  -e KEYCLOAK_USER=admin \
-  -e KEYCLOAK_PASSWORD=admin \
   -e KEYCLOAK_IMPORT=/tmp/realms/example.generated.json \
   -v $(pwd)/build_keycloak:/tmp/build_keycloak \
   -v $(pwd)/tools/config/cli:/opt/jboss/startup-scripts \
   -v $(pwd)/tools/config/realms:/tmp/realms \
   -p 8080:8080 \
   jboss/keycloak:16.0.0
+
